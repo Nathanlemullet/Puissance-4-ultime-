550e622f04d673e577d66a6c5da24c0c4149b181
guilh <guilh@192.168.114.81>
2021-11-15 15:00:37 +0100

    joueur version 1.1

--- src/sp4_console_michelland_peron/Grille.java
+++ src/sp4_console_michelland_peron/Grille.java
@@ -9,12 +9,5 @@ package sp4_console_michelland_peron;
  * @author miche
  */
 public class Grille {
-    Cellule [][] cellulesjoue = new Cellule[6][7];
     
-public Grille (){
-    for (int i=0, i<6 , i++){
-        
-    }
-        
-}
 }
