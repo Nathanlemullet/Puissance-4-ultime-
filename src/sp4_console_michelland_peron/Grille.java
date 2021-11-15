619fd0bf9c63418b109d5c5c812ff4d23b4ae008
miche <miche@192.168.114.86>
2021-11-15 15:02:55 +0100

    Grille cellule V1.1

--- src/sp4_console_michelland_peron/Grille.java
+++ src/sp4_console_michelland_peron/Grille.java
@@ -9,12 +9,43 @@ package sp4_console_michelland_peron;
  * @author miche
  */
 public class Grille {
-    Cellule [][] cellulesjoue = new Cellule[6][7];
-    
-public Grille (){
-    for (int i=0, i<6 , i++){
-        
-    }
-        
+    Cellule [][] CellulesJeu = new Cellule [6][7];
+
+
+public void grille (){
+
 }
+
+public boolean ajouterJetonDansColonne(Jeton Jeton, int nbCol){
+    int i;
+    for (i=0; i<7;i++){
+        if (CellulesJeu[nbCol][i].jetonCourant==null){
+            return true;
+        }
+    }
+    return false;
+    
+}
+
+public boolean etreRemplie(){
+    int i;
+    int j;
+    for (i=0; i<7;i++){
+        for (j=0; j<6;j++){
+            if (CellulesJeu[j][i]==null){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+public void viderGrille(){
+    CellulesJeu= null;
+}
+
+public void afficherGrilleSurConsole(){
+    
+}
+
 }
